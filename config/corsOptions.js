c2623b06b12b20af33cc3dba297c6182460e418b
Sarvesh <coolsarveshkirodiwal111@gmail.com>
2023-02-27 02:34:59 +0530

    cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,7 @@
 const corsOptions = {
   origin: process.env.URL,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  methods: "GET,PUT,POST,DELETE ",
 };
 
 module.exports = corsOptions;
