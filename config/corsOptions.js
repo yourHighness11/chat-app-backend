0143d516a763934ea09ce8e297e26f5e75909f5b
yourHeighness <113287526+yourHighness11@users.noreply.github.com>
2025-01-17 01:28:07 +0530

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: process.env.URL,
+  origin: "https://chat-app-personal-394e5.web.app",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   methods: "GET,PUT,POST,DELETE ",
 };
