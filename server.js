d405ef8438b1a1cd637e437cdb4749ccd08b6aae
yourHeighness <113287526+yourHighness11@users.noreply.github.com>
2025-01-19 18:39:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const messageRoutes = require("./routes/messageRoutes");
 const corsOptions = require("./config/corsOptions");
 // added comment Data
 connectDB();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
